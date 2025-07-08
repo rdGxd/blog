ee565d3f2e0fb9918936b71f29d05e98135b3d6b
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-08 11:21:49 -0300

    Header Component + Breakpoints do Tailwind CSS com mobile-first

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,11 +2,10 @@
 
 export function Header() {
   return (
-    <h1
-      className="text-6xl font-bold text-blue-500 hover:text-blue-50 hover:bg-blue-500 transition duration-1000"
-      onClick={() => alert("Olá mundo!")}
-    >
-      Olá mundo!
-    </h1>
+    <header>
+      <h1 className="text-4xl/normal font-extrabold py-8 sm:text-5xl/normal sm:py-10 md:text-6xl/normal md:py-11 lg:text-7xl/normal lg:py-12 ">
+        <a href="#">The Blog</a>
+      </h1>
+    </header>
   );
 }
