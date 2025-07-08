de5e0b405024c11e2dbedd951127cc02d6aaabf0
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-08 10:58:56 -0300

    Componente Container + Dicas do Tailwind CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import Container from "@/components/Container";
 import PostsLists from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
+    <Container>
       <header>
         <h1 className="text-6xl font-bold text-center py-8">Blog</h1>
       </header>
+
       <main>
         <Suspense fallback={<SpinLoader />}>
           <PostsLists />
@@ -16,6 +18,6 @@ export default async function HomePage() {
       <footer className="text-center py-4">
         <p className="text-gray-500">© 2023 My Blog</p>
       </footer>
-    </div>
+    </Container>
   );
 }
