befd193e9c78bbf0a0211b058e5531cf7235f06f
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-08 09:25:04 -0300

    Padrão de projeto Repository para Posts - Parte 2

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,14 +1,34 @@
 import { PostModel } from "@/models/post/post-model";
+import { readFile } from "fs/promises";
+import { resolve } from "path";
 import { PostRepository } from "./post-repository";
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  "src",
+  "db",
+  "seed",
+  "posts.json"
+);
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
 
   async findAll(): Promise<PostModel[]> {
-    throw new Error("Method not implemented.");
+    const posts = await this.readFromDisk();
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find((post) => post.id === id);
+    if (!post) throw new Error(`Post with id ${id} not found`);
+    return post;
   }
 }
-
-export const postRepository = new JsonPostRepository();
