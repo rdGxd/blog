a9acfdc7df24f024c9989eef6e46029e8316fc99
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-09 09:51:12 -0300

    Componente FeaturedPost + Tailwind CSS Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,32 @@ import Container from "@/components/Container";
 import { Header } from "@/components/Header";
 import PostsLists from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group" >
+        <Link href="#" className="w-full h-full overflow-hidden rounded-xl">
+          <Image
+            className="group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            alt="Description of image"
+            width={1200}
+            height={720}
+          />
+        </Link>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit ab
+          dolorum nobis non aperiam nam, exercitationem accusantium vel
+          obcaecati sint sit ipsum molestias! Beatae recusandae harum, aliquid
+          necessitatibus eos enim.
+        </div>
+      </section>
 
       <main>
         <Suspense fallback={<SpinLoader />}>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import Link from "next/link";
+
 export function Header() {
   return (
     <header>
       <h1 className="text-4xl/normal font-extrabold py-8 sm:text-5xl/normal sm:py-10 md:text-6xl/normal md:py-11 lg:text-7xl/normal lg:py-12 ">
-        <a href="#">The Blog</a>
+        <Link href="#">The Blog</Link>
       </h1>
     </header>
   );
