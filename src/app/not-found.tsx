0768da1d4719c5c635538edb1e6e0afaf3dfb77e
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-15 11:43:34 -0300

    Componente para as mensagens de erro para o cliente (ErrorMessage)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,11 @@
+import ErrorMessage from '@/components/ErrorMessage';
+
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada</title>
-      <div className='mb-16 flex min-h-[320px] items-center justify-center rounded-xl bg-slate-900 p-8 text-center text-slate-100'>
-        <div>
-          <h1 className='mb-4 text-7xl/tight font-extrabold'>404</h1>
-          <p>Error 404 - A página que você está procurando não existe.</p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle='Página não encontrada'
+      contentTitle='404'
+      content='Error 404 - A página que você está procurando não existe.'
+    />
   );
 }
---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,23 @@
+type ErrorMessageProps = {
+  pageTitle: string;
+  contentTitle: string;
+  content: React.ReactNode;
+};
+
+export default function ErrorMessage({
+  pageTitle,
+  contentTitle,
+  content,
+}: ErrorMessageProps) {
+  return (
+    <>
+      <title>{pageTitle}</title>
+      <div className='mb-16 flex min-h-[320px] items-center justify-center rounded-xl bg-slate-900 p-8 text-center text-slate-100'>
+        <div>
+          <h1 className='mb-4 text-7xl/tight font-extrabold'>{contentTitle}</h1>
+          <div>{content}</div>
+        </div>
+      </div>
+    </>
+  );
+}
