58e265d1ce157f3482bc347552abeea76a296cdc
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-15 10:18:01 -0300

    generateMetadata - Gerando title, description ou outras metas dinâmicamente

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,13 @@
 export default function NotFoundPage() {
   return (
-    <div className="min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
-      <div>
-        <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
-        <p>Error 404 - A página que você está procurando não existe.</p>
+    <>
+      <title>Página não encontrada</title>
+      <div className="min-h-[320px] bg-slate-900 text-slate-100 mb-16 p-8 rounded-xl flex items-center justify-center text-center">
+        <div>
+          <h1 className="text-7xl/tight mb-4 font-extrabold">404</h1>
+          <p>Error 404 - A página que você está procurando não existe.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,20 @@
 import { findPostsBySlugCached } from "@/lib/post/queries";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findPostsBySlugCached(slug);
+  return {
+    title: `${post.title}`,
+    description: post.excerpt,
+  };
+}
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
