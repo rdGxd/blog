6d647acd47017436df318c20410b9b63473c3822
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-23 10:15:14 -0300

    Estilizando o MenuAdmin com Tailwind CSS

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,13 +1,46 @@
-import { Link } from 'lucide-react';
+import { FileTextIcon, HouseIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export function MenuAdmin() {
+  const navClasses =
+    'bg-slate-900 text-slate-100 rounded-lg flex flex-col overflow-hidden mb-8 sm:flex-row sm:flex-wrap';
+  const linkClasses =
+    '[&>svg]:w-[16px] [&>svg]:h-[16px] flex items-center gap-2 px-4 hover:bg-slate-800 flex transition hover:bg-slate-800 justify-start h-10 shrink-0 rounded-lg';
+
   return (
-    <nav>
-      <a href='/' target='_blank'>
+    <nav className={navClasses}>
+      <a href='/' target='_blank' className={linkClasses}>
+        <HouseIcon />
         Home
       </a>
-
-      <Link href='/admin/post'>Posts</Link>
+      <Link href='/admin/post' className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link href='/admin/post' className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link href='/admin/post' className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link href='/admin/post' className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>{' '}
+      <Link href='/admin/post' className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link href='/admin/post' className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link href='/admin/post' className={linkClasses}>
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
