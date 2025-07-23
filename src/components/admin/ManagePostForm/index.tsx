ba1a9a4ab9180e596aab36fadc1b720582bb3de6
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-23 11:58:59 -0300

    Formulário de criação e edição do post (ManagePostForm)

---
+++ src/components/admin/ManagePostForm/index.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { Button } from '@/components/Button';
+import { InputCheckbox } from '@/components/InputCheckbox';
+import { InputText } from '@/components/InputText';
+
+export function ManagePostForm() {
+  return (
+    <form action='' className='mb-16'>
+      <div className='flex flex-col gap-6'>
+        <InputText placeholder='Digite seu nome' labelText='Nome' />
+        <InputText placeholder='Digite seu email' labelText='Email' />
+        <InputCheckbox labelText='Aceito os termos' />
+      </div>
+      <div className='mt-6'>
+        <Button variant={'default'} size='lg' className='w-full' type='submit'>
+          Enviar
+        </Button>
+      </div>
+    </form>
+  );
+}
