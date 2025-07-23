22bbac070ba73c05f696fba174c7d8bf8455214f
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-23 11:07:42 -0300

    Usando TypeScript para adicionar variações de estilo no componente

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -4,8 +4,16 @@ export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div>
-      <Button className='cursor-pointer'>Create Post</Button>
+    <div className='flex flex-wrap gap-4 py-16'>
+      <Button className='cursor-pointer' variant='danger' size='small'>
+        Confirma
+      </Button>
+      <Button className='cursor-pointer' variant='default' size='medium'>
+        Confirma
+      </Button>
+      <Button className='cursor-pointer' variant='ghost' size='large'>
+        Confirma
+      </Button>
     </div>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,34 @@
-type ButtonProps = {} & React.ComponentProps<'button'>;
+type ButtonVariants = 'default' | 'ghost' | 'danger';
 
-export function Button({ children, ...props }: ButtonProps) {
+type ButtonSizes = 'sm' | 'md' | 'lg';
+
+type ButtonProps = {
+  variant: ButtonVariants;
+  size: ButtonSizes;
+} & React.ComponentProps<'button'>;
+
+export function Button({
+  variant = 'default',
+  size = 'md',
+  ...props
+}: ButtonProps) {
+  const variantClasses: Record<ButtonVariants, string> = {
+    default: 'bg-blue-600 text-blue-100 hover:bg-blue-700',
+    ghost: 'bg-slate-200 text-blue-900 hover:bg-slate-500',
+    danger: 'bg-red-500 text-white hover:bg-red-600',
+  };
+
+  const sizeClasses: Record<ButtonSizes, string> = {
+    sm: 'text-xs py-1 px-2',
+    md: 'text-sm py-2 px-4',
+    lg: 'text-lg py-3 px-6',
+  };
+
+  const buttonClasses = `${variantClasses[variant]} ${sizeClasses[size]} `;
+
   return (
     <>
-      <button {...props}>{children}</button>
+      <button {...props} className={`${buttonClasses} rounded-md`} />
     </>
   );
 }
