99334444180bd2a803f0f4ec2261217e5f08acec
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-24 09:59:01 -0300

    Criando o editor Markdown com @uiw/react-md-editor

---
+++ src/components/MarkdownEditor/index.tsx
@@ -0,0 +1,56 @@
+import dynamic from 'next/dynamic';
+import { useId } from 'react';
+import rehypeSanitize from 'rehype-sanitize';
+import remarkGfm from 'remark-gfm';
+
+const MDEditor = dynamic(() => import('@uiw/react-md-editor'), {});
+
+type MarkdownEditorProps = {
+  labelText?: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  textAreaName: string;
+  disabled?: boolean;
+};
+
+export function MarkdownEditor({
+  labelText,
+  value,
+  setValue,
+  textAreaName,
+  disabled,
+}: MarkdownEditorProps) {
+  const id = useId();
+
+  return (
+    <div className='flex flex-col gap-2'>
+      {labelText && (
+        <label htmlFor={id} className='text-sm'>
+          {labelText}
+        </label>
+      )}
+      <MDEditor
+        className='whitespace-pre-wrap'
+        id={id}
+        value={value}
+        onChange={value => {
+          if (value === undefined) return;
+          setValue(value);
+        }}
+        height={400}
+        // extraCommands={[]}
+        preview='edit'
+        hideToolbar={disabled}
+        textareaProps={{
+          id,
+          name: textAreaName,
+          disabled: disabled,
+        }}
+        previewOptions={{
+          rehypePlugins: [[rehypeSanitize]],
+          remarkPlugins: [[remarkGfm]],
+        }}
+      />
+    </div>
+  );
+}
--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -3,14 +3,25 @@
 import { Button } from '@/components/Button';
 import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
+import { MarkdownEditor } from '@/components/MarkdownEditor';
+import { useState } from 'react';
 
 export function ManagePostForm() {
+  const [content, setContent] = useState('');
+
   return (
     <form action='' className='mb-16'>
       <div className='flex flex-col gap-6'>
         <InputText placeholder='Digite seu nome' labelText='Nome' />
         <InputText placeholder='Digite seu email' labelText='Email' />
         <InputCheckbox labelText='Aceito os termos' />
+        <MarkdownEditor
+          labelText='Conteúdo'
+          value={content}
+          disabled={false}
+          setValue={setContent}
+          textAreaName='content'
+        />
       </div>
       <div className='mt-6'>
         <Button variant={'default'} size='lg' className='w-full' type='submit'>
