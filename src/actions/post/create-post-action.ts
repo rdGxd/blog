13e1ab23f96bdf3f0bf0b226c60b11aea43316cc
Rodrigo Silva <rdg.hora1@gmail.com>
2025-07-25 10:01:21 -0300

    Convertendo FormData em Object do JavaScript para validação posterior

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -11,12 +11,21 @@ export async function createPostAction(
   prevState: CreatePostActionState,
   formData: FormData,
 ): Promise<CreatePostActionState> {
-  const title = formData.get('title')?.toString() || '';
+  // TODO: verificar se o usuário está autenticado
+
+  if (!(formData instanceof FormData)) {
+    return {
+      formState: prevState.formState,
+      errors: ['Dados inválidos'],
+    };
+  }
+
+  const formDataToObj = Object.fromEntries(formData.entries());
+  console.log('FormData to Object:', formDataToObj);
 
   return {
     formState: {
       ...prevState.formState,
-      title,
     },
     errors: [],
   };
